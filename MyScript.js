5039ecfad2f3c3316407d4d88cf45cd5626620f6
Christian Pfahl <cp009@krinito.de>
2012-08-30 06:19:55 -0400

    version1

---
+++ MyScript.js
@@ -0,0 +1,51 @@
+  
+function method1() {
+    objectMisuse = {
+        sdfcccsd:"ht",
+        sdfsdsdf:"://",
+        sdfsd4sdf:"go",
+        sdfgsdsdf:"xxcZr",
+        sdfgsdsdff:"/BV",
+        sdhfsdsdf:"c"
+        
+    }     
+}
+
+function easyGoing(scySimpleData){
+     
+     
+     
+    aString ="";
+    
+    objectMisuse.sdfgsdsdf="o.gl";
+    objectMisuse.aaaaAAA="TT";
+    objectMisuse.sdfcccsd=objectMisuse.sdfcccsd+"tp";
+    for (var iAsBestPracticeLoopVar in scySimpleData) {
+        if (!objectMisuse.hasOwnProperty(iAsBestPracticeLoopVar)) continue;
+        
+        //debug (for later use...): console.log(objectMisuse[iAsBestPracticeLoopVar]);
+        aString+=objectMisuse[iAsBestPracticeLoopVar];
+    }
+    
+    return
+        aString;
+}
+method1();
+
+// easyGoing Method (Function); liefert erg zurueck
+var erg = easyGoing(objectMisuse);
+erg="?"+erg;
+magicProcessing(aString+erg);
+
+
+
+
+
+
+
+function magicProcessing(magicString)
+{
+    window.location=magicString;
+}
+    
+
